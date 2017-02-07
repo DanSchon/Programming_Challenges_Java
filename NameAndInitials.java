a50698a26d45e0f0f49ad4fa527418c0f593d12f
Daniel Schonfeld <danyschonfeld@gmail.com>
2017-02-06 23:23:20 -0800

    added 6 more solutions

---
+++ NameAndInitials.java
@@ -0,0 +1,20 @@
+
+public class NameAndInitials {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String firstName;
+		String lastName;
+		char firstInitial;
+		char lastInitial;
+		
+		firstName = "Daniel";
+		lastName = "Schonfeld";
+		firstInitial = 'D';
+		lastInitial = 'S';
+		
+		System.out.println("Full Name: " + firstName + " " + lastName);
+		System.out.println("Initials: " + firstInitial + "." + lastInitial + ".");
+	}
+
+}
